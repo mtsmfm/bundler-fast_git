a833952bd5e387bd64e33a502d383ac2af87cc4b
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-06-27 18:43:13 +0000

    Impl

--- lib/bundler/fast_git_source.rb
+++ lib/bundler/fast_git_source.rb
@@ -6,3 +6,15 @@ module Bundler
     # Your code goes here...
   end
 end
+
+Bundler::Source::Git::GitProxy.prepend(Module.new do
+  def git(command, *args)
+    if command.start_with?('clone')
+      super(command.sub('--no-hardlinks', '') + ' --depth=1', *args)
+    elsif command.start_with?('fetch')
+      super(command + ' --depth=1', *args)
+    else
+      super
+    end
+  end
+end)
