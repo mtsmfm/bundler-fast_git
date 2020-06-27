04cbbd8d3c4ea525b37ed0cc8c234b9ee6e81773
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2020-06-27 19:40:29 +0000

    Impl as source

--- lib/bundler/fast_git_source.rb
+++ lib/bundler/fast_git_source.rb
@@ -2,19 +2,27 @@ require "bundler/fast_git_source/version"
 
 module Bundler
   module FastGitSource
+    class Source < Bundler::Source::Git
+    end
+
+    class GitProxy < Bundler::Source::Git::GitProxy
+      def git(command, *args)
+        Bundler.ui.debug(command)
+        if command.start_with?('clone')
+          super(command.sub('--no-hardlinks', '') + ' --depth=1', *args)
+        elsif command.start_with?('fetch')
+          super(command + ' --depth=1', *args)
+        else
+          super
+        end
+      end
+    end
+
+    class Plugin < Bundler::Plugin::API
+      source "fast_git_source"
+    end
+
     class Error < StandardError; end
     # Your code goes here...
   end
 end
-
-Bundler::Source::Git::GitProxy.prepend(Module.new do
-  def git(command, *args)
-    if command.start_with?('clone')
-      super(command.sub('--no-hardlinks', '') + ' --depth=1', *args)
-    elsif command.start_with?('fetch')
-      super(command + ' --depth=1', *args)
-    else
-      super
-    end
-  end
-end)
--- lib/bundler/fast_git_source/version.rb
+++ lib/bundler/fast_git_source/version.rb
@@ -1,5 +1,5 @@
 module Bundler
   module FastGitSource
-    VERSION = "0.1.1"
+    VERSION = "0.2.0"
   end
 end
